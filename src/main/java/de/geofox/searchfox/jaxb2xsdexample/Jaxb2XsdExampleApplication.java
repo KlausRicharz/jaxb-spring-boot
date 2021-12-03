2efb2e219f04ce7ce338ad984b3ccbb8dc583225
Klaus Richarz <kr@hbt.de>
2021-12-03 12:26:29 +0100

    init commit

---
+++ src/main/java/de/geofox/searchfox/jaxb2xsdexample/Jaxb2XsdExampleApplication.java
@@ -0,0 +1,13 @@
+package de.geofox.searchfox.jaxb2xsdexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaxb2XsdExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Jaxb2XsdExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/geofox/searchfox/jaxb2xsdexample/Jaxb2XsdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.geofox.searchfox.jaxb2xsdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaxb2XsdExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
